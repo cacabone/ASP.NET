fcb57091b6247335d97195e9f58645edc26e0ad3
Robert Nuñez <robertgabrielnunezmatias@gmail.com>
2024-12-07 16:05:23 -0400

    ASP.NET

---
+++ ASP.NET/Models/Student.cs
@@ -0,0 +1,13 @@
+﻿namespace ASP.NET.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Age { get; set; }
+    }
+}
